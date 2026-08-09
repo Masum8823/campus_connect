30b4f17551c3e65b11e32268b534884088321a35
Masum8823 <masum688823@gmail.com>
2026-08-09 06:48:02 +0600

    Feat: Implement item deletion with automatic storage cleanup for marketplace

---
+++ marketplace/delete_item.php
@@ -0,0 +1,19 @@
+<?php
+include '../config.php';
+session_start();
+
+if(isset($_GET['id']) && isset($_SESSION['user_id'])){
+    $id = $_GET['id'];
+    $u_id = $_SESSION['user_id'];
+    
+    // ছবি ডিলিট করা
+    $img_q = mysqli_query($conn, "SELECT item_image FROM marketplace_items WHERE id='$id' AND seller_id='$u_id'");
+    $img_data = mysqli_fetch_assoc($img_q);
+    if($img_data['item_image'] != 'uploads/marketplace/no_product.png' && file_exists("../".$img_data['item_image'])){
+        unlink("../".$img_data['item_image']);
+    }
+
+    mysqli_query($conn, "DELETE FROM marketplace_items WHERE id='$id' AND seller_id='$u_id'");
+}
+header("Location: index.php");
+exit();
